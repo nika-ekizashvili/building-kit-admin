8cf7e602ade5a43936d8787abeeeaaed89a24e8b
whatever <eqiza99@gmail.com>
2023-08-29 14:37:01 +0400

    update

--- src/plugins/tbc-payment-api/server/services/payment.js
+++ src/plugins/tbc-payment-api/server/services/payment.js
@@ -1,7 +1,27 @@
-'use strict';
+"use strict";
 
 module.exports = ({ strapi }) => ({
-    async find(query) {
-        return await strapi.entityService.findMany("plugin::payment.payment", query)
-    }
+  async find(query) {
+    return await strapi.entityService.findMany("plugin::payment.payment", query);
+  },
+
+  async delete(id) {
+    return await strapi.entityService.delete("plugin::payment.payment", id);
+  },
+
+  async create(data) {
+    return await strapi.entityService.create("plugin::payment.payment", data);
+  },
+
+  async update(id, data) {
+    return await strapi.entityService.update("plugin::payment.payment", id, data);
+  },
+
+  async toggle(id) {
+    const result = await strapi.entityService.findOne("plugin::payment.payment", id);
+
+    return await strapi.entityService.update("plugin::payment.payment", id, {
+      data: { isDone: !result.isDone },
+    });
+  },
 });
